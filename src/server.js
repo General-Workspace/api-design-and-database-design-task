fc81ae72466943c1ed19c50cf0e1a526996ddbd9
aniebietafia <aniebietafia87@gmail.com>
2024-07-13 12:07:57 +0100

    documemtation

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 import http from "node:http";
 import app from "./app.js";
 
-
 const port = process.env["PORT"] || 1234;
 
 const server = http.createServer(app);
 
 const start = () => {
-    server.listen(port, () => {
-        console.log("Server has started.");
-    });
-}
+  server.listen(port, () => {
+    console.log(`Server has started on http://localhost:${port}`);
+  });
+};
 
 start();
